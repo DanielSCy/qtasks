1eab4b37fa6664cf8973cbfdd3fbce2fe97e4eac
dstanciu@gmail.com <dstanciu@gmail.com@83237b03-c084-ea1e-c160-27af89a6138e>
2012-10-09 08:35:43 +0000

    - added light/dark theme option - added up button on the settings screen

--- trunk/src/daniel/stanciu/quicktasks/SettingsActivity.java
+++ trunk/src/daniel/stanciu/quicktasks/SettingsActivity.java
@@ -5,30 +5,38 @@ import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.os.Bundle;
 import android.preference.ListPreference;
 import android.preference.PreferenceActivity;
+import android.support.v4.app.NavUtils;
+import android.view.MenuItem;
 
 public class SettingsActivity extends PreferenceActivity implements OnSharedPreferenceChangeListener {
 	public static String NEW_TASK_PRIO_PREF_KEY = null;
+	public static String THEME_PREF_KEY = null;
 	@SuppressWarnings("deprecation")
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
 		addPreferencesFromResource(R.xml.preferences);
 		if (NEW_TASK_PRIO_PREF_KEY == null) {
 			NEW_TASK_PRIO_PREF_KEY = getString(R.string.new_task_prio_pref_key);
 		}
+		if (THEME_PREF_KEY == null) {
+			THEME_PREF_KEY = getString(R.string.theme_pref_key);
+		}
 		onSharedPreferenceChanged(getPreferenceScreen().getSharedPreferences(), NEW_TASK_PRIO_PREF_KEY);
+		onSharedPreferenceChanged(getPreferenceScreen().getSharedPreferences(), THEME_PREF_KEY);
 	}
 
 	@SuppressWarnings("deprecation")
 	@Override
 	public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
-		if (key.equals(NEW_TASK_PRIO_PREF_KEY)) {
-			ListPreference newTaskPrio = (ListPreference)findPreference(key);
-			int valueIndex = newTaskPrio.findIndexOfValue(sharedPreferences.getString(key, ""));
+		if (key.equals(NEW_TASK_PRIO_PREF_KEY) || key.equals(THEME_PREF_KEY)) {
+			ListPreference newPref = (ListPreference)findPreference(key);
+			int valueIndex = newPref.findIndexOfValue(sharedPreferences.getString(key, ""));
 			if (valueIndex != -1) {
-				newTaskPrio.setSummary(newTaskPrio.getEntries()[valueIndex]);
+				newPref.setSummary(newPref.getEntries()[valueIndex]);
 			} else {
-				newTaskPrio.setSummary("");
+				newPref.setSummary("");
 			}
 		}
 	}
@@ -47,4 +55,18 @@ public class SettingsActivity extends PreferenceActivity implements OnSharedPref
 		getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
 	}
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    
+    @Override
+    public void onBackPressed() {
+        NavUtils.navigateUpFromSameTask(this);
+    }
 }
